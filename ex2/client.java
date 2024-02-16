d85f972d8f7307e3f7c3f975749e1455de3a3cdb
oussemaha <oussemahamouda6@gmail.com>
2024-02-16 23:11:04 +0100

    ex2 done

--- ex2/client.java
+++ ex2/client.java
@@ -1,6 +1,5 @@
 package ex2;
 
-import ex2.Voiture;
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
@@ -8,9 +7,10 @@ import java.util.Scanner;
 class client {
     public static void main(String argv[]) {
         int port = 0;
-        String host = "";
+        Voiture karhabti;
+        String host = "",typeV="",modelV="";
         Scanner keyb = new Scanner(System.in);
-        // .............................................................
+        
         System.out.print("Nom du serveur : ");
         host = keyb.next();
         System.out.print("Port du serveur : ");
@@ -21,18 +21,17 @@ class client {
             System.err.println("Le second paramètre n'est pas un entier.");
             System.exit(-1);
         }
-        // .............................................................
+        System.out.println("type de Voiture :");
+        typeV = keyb.next();
+        System.out.println("modèle de Voiture :");
+        modelV = keyb.next();
+        karhabti= new Voiture(typeV, modelV);
         try {
-            // .............................................................
             InetAddress adr = InetAddress.getByName(host);
-            // .............................................................
             Socket socket = new Socket(adr, port);
-            // .............................................................
             ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-            // .............................................................
-            output.writeObject(new String("ma première socket"));
-            // .............................................................
+            output.writeObject(karhabti);
             String chaine = (String) input.readObject();
             System.out.println(" recu du serveur : " + chaine);
         } catch (Exception e) {
--- ex2/server.java
+++ ex2/server.java
@@ -1,19 +1,14 @@
 package ex2;
 
-import ex2.Voiture;
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
 class server {
     public static void main(String argv[]) {
-        Voiture karahbti=new Voiture("volswagen","Golf");
-        int port = 0;
-        String host = "";
+        Voiture karhabti;
+        int port = 0,carburant;
         Scanner keyb = new Scanner(System.in);
-        // .............................................................
-        System.out.print("Nom du serveur : ");
-        host = keyb.next();
         System.out.print("Port du serveur : ");
 
         try {
@@ -22,21 +17,27 @@ class server {
             System.err.println();
             System.exit(-1);
         }
-        // .............................................................
+
         try {
-            // .............................................................
-            InetAddress adr = InetAddress.getByName(host);
-            // .............................................................
-            Socket socket = new Socket(adr, port);
-            // .............................................................
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket socket = serverSocket.accept();
             ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-            // .............................................................
-            output.writeObject(new String("ma première socket"));
-            // .............................................................
-            String chaine = (String) input.readObject();
-            System.out.println(" recu du serveur : " + chaine);
-            socket.close();
+            karhabti = (Voiture) input.readObject();
+            System.out.println(" une voiture a été reçue " );
+            System.out.println(" ca vient de : " + socket.getInetAddress() +
+                    ":" + socket.getPort());
+            System.out.println("choisir la quantité à remplir dans le réservoir:");
+            try {
+                carburant = keyb.nextInt();
+                karhabti.setCarburant(carburant);
+            } catch (NumberFormatException e) {
+                System.err.println();
+                System.exit(-1);
+            }
+
+            output.writeObject(new String("bien recu"));
+            serverSocket.close();
         } catch (Exception e) {
             System.err.println("Erreur : " + e);
         }
